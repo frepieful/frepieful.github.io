b9c43347a8d57768eb6f0b6497de1a829ecf705b
frepieful <freicia07@gmail.com>
2016-02-14 22:40:06 -0500

    Reto 3 Hackspace
    Reto 3 Hackspace

---
+++ public3/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider({full_width: true});
+    $('.carousel1').carousel();
+    $('.carousel2').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
